b8d8796226cce9d6693b02a5bd68ea1f518f3426
Mona Lisa <it22348098@my.sliit.lk>
2026-04-24 14:23:08 +0530

    Signup Activation

--- admin/src/app/(full-width-pages)/(auth)/signup/page.tsx
+++ admin/src/app/(full-width-pages)/(auth)/signup/page.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from "next";
 import SignUpForm from "@/components/auth/SignUpForm";
 export const metadata: Metadata = {
-  title: "Sign up disabled",
-  description: "Sign up is currently disabled for this application.",
+  title: "Sign up ",
+  description: "Sign up is available for this application.",
 };
 
 export default function SignUp() {
